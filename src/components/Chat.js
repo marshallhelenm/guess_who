d7c02ab06692fac91e6c74ca0658a3e79367b430
marshallhelenm <marshallhelenm@gmail.com>
2020-05-19 12:55:52 -0700

    wrote some more for components, added socketio-client. having cors issues.

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client"
+import "../css/chat.css";
 
-const Chat = () => {
-  return(<h1>Chat Component</h1>)
+const ENDPOINT = "http://localhost:5000/"
+
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    console.log(name, room);
+
+    socket = io(ENDPOINT+"join")
+
+    setName(name)
+    setRoom(room)
+
+    console.log(socket)
+  }, [ENDPOINT, location.search]);
+  return (
+  <h1>Chat Component</h1>
+  );
 };
 
 export default Chat;
