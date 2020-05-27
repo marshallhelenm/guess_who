a654439a5aec313493756d2190ae540543e9ba36
marshallhelenm <marshallhelenm@gmail.com>
2020-05-27 16:08:14 -0700

    users and messaging

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -10,6 +10,8 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -21,11 +23,41 @@ const Chat = ({ location }) => {
     setRoom(room);
 
     console.log(socket);
-    socket.emit("join", { name, room }, ({error})=>{
-      alert(error)
-    });
+    socket.emit("join", { name, room }, () => {});
+
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
   }, [ENDPOINT, location.search]);
-  return <h1>Chat Component</h1>;
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  //TODO: function for sending messages
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+  console.log('message: ', message)
+  console.log('messages: ', messages)
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => (e.key === "Enter" ? sendMessage(e) : null)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
